82bfe4a9652b17ac52d4be1ac16e0908d92b6af4
Jared Graham <grahamjared5@gmail.com>
2019-05-05 13:36:56 -0400

    Move registration into `register` module

---
+++ src/register.py
@@ -0,0 +1,19 @@
+import sys
+from git import Repo
+
+# what we're doing in git terms:
+#
+# git config alias.git_command_name '!"<filepath_python>" "<filepath_script>"'
+#
+# english:
+#
+# Create a .bat script 'in place' with the alias to call a python script.
+# The python script can then run any amount of git commands or do whatever
+# it wants.
+
+def command(name, filepath_python_script, filepath_repository):
+    CONST_BATCH_SCRIPT = '!"' + sys.executable + '" "' + filepath_python_script + '"'
+    # get repo
+    repo = Repo(filepath_repository)
+    git  = repo.git
+    git.execute(['git', 'config', 'alias.' + name, CONST_BATCH_SCRIPT])
