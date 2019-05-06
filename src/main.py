beb2eb3255554b9d61bede5ccc45c104816d1a54
Jared Graham <grahamjared5@gmail.com>
2019-05-05 23:58:40 -0400

    Rename variable

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import register
 
-git_command_name    = input("enter the desired custom name: ")
 filepath_script     = input("enter the filepath of the python script you want to call through git\n(e.g. C:/Scripts/destroy_repo.py): ")
 filepath_repository = input("git repo filepath, where one might find a .git folder.\n(e.g. C:/repositories/myproject): ")
 
 
-register.command(git_command_name, filepath_script, filepath_repository)
+name                = input("enter the desired custom name: ")
+register.command(name, filepath_script, filepath_repository)
